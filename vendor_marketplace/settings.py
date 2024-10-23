bfb7e38b41dc105a2d38a54fdd3843790ff4129a
nahloo-obi <naluprosper54@gmail.com>
2024-10-23 12:57:17 +0100

    added template directory

--- vendor_marketplace/settings.py
+++ vendor_marketplace/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'vendor_marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
