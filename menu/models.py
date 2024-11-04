d3814347a9d8ec5593d3fe5a742d0a9bb69f60ae
nahloo-obi <naluprosper54@gmail.com>
2024-11-04 13:22:06 +0000

    added menu functionality

---
+++ menu/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from vendor.models import Vendor
+# Create your models here.
+
+class Category(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=250, blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.category_name
+    
+class Item(models.Model):
+    Vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    item_title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=250, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='itemimages')
+    is_available =models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.item_title
+
--- vendor_marketplace/settings.py
+++ vendor_marketplace/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'vendor'
+    'vendor',
+    'menu',
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
