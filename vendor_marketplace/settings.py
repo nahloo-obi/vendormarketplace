b6f034fa19fca30a6dd3a73d056ca291b75933e6
nahloo-obi <naluprosper54@gmail.com>
2025-01-06 22:08:05 +0000

    configured whitenoise

--- vendor_marketplace/settings.py
+++ vendor_marketplace/settings.py
@@ -147,6 +147,10 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    
    "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
