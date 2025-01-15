a4756bf7906a8cff699ed978638ccad3ef121f24
nahloo-obi <naluprosper54@gmail.com>
2025-01-15 11:25:56 +0000

    refactor

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1342,10 +1342,8 @@
         return this })), window.Swiper = t
 }(), "undefined" != typeof module ? module.exports = window.Swiper : "function" == typeof define && define.amd && define([], function() { "use strict";
     return window.Swiper });
-//# sourceMappingURL=maps/swiper.min.js.map
 
 
-// bootstrap-datepicker.js
 
 
 //! moment.js
