46cf4597e14949dd5aff0755f31da00b1073dda3
nahloo-obi <naluprosper54@gmail.com>
2025-01-24 12:25:30 +0000

    updated the script file

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,4 +6,5 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 echo "Migrations has been done"
-uwsgi --socket :9000 --workers 4 --master --enable-threads --module vendor_marketplace.wsgi
+gunicorn --workers 4 --threads 2 --bind :9000 vendor_marketplace.wsgi:application
+# uwsgi --socket :9000 --workers 4 --master --enable-threads --module vendor_marketplace.wsgi
